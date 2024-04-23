63c1fb2648c95c6f2397269fa49676d2e9bc30ff
fsyrachel <fengsiyu0207@gmail.com>
2024-04-23 22:25:52 +0800

    将home页C优化：使得跳转的新界面创建时候默认不打开

--- VirtualBankV1/src/main/java/com/virtualbankv1/control/HomePageController.java
+++ VirtualBankV1/src/main/java/com/virtualbankv1/control/HomePageController.java
@@ -14,20 +14,19 @@ public class HomePageController {
     }
 
     public void initializeController() {
-        // 为view中的按钮添加事件监听器。
-        //addReturnListenerToButton1(view.getAccountButton(), new AccountOverviewPage());
+        // 为view中的按钮添加事件监听器
+        //addReturnListenerToButton(view.getAccountButton(), new AccountOverviewPage());
         //addReturnListenerToButton(view.getTasksButton(), new testpage2("VB"));
         //addReturnListenerToButton(view.getGoalsButton(), new testpage3("VB"));
         //addReturnListenerToButton(view.getManualButton(), new testpage4("VB"));
     }
 
-    private void addReturnListenerToButton1(JButton button, JFrame newPage) {
-        newPage.setVisible(false);
+    private void addReturnListenerToButton(JButton button, JFrame newPage) {
+        newPage.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 view.setVisible(false);
-                newPage.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE); // 点击关闭时隐藏
                 newPage.setVisible(true);
             }
         });
