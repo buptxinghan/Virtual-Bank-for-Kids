d17fe560ccfb47fcadcc73ae732941e02ef5132c
fsyrachel <fengsiyu0207@gmail.com>
2024-04-25 19:49:03 +0800

    Adding variable and method annotations to home and homepage

--- VirtualBankV1/src/main/java/com/virtualbankv1/control/HomePageController.java
+++ VirtualBankV1/src/main/java/com/virtualbankv1/control/HomePageController.java
@@ -9,18 +9,30 @@ import java.awt.event.ActionListener;
 public class HomePageController {
     private HomePage view;
 
+    /**
+     * Constructor for HomePageController class.
+     * @param view The view associated with the controller.
+     */
     public HomePageController(HomePage view) {
         this.view = view;
     }
 
+    /**
+     * Initializes the controller by adding event listeners to buttons in the view.
+     */
     public void initializeController() {
-        // 为view中的按钮添加事件监听器
+        // Adds event listeners to buttons in the view
         //addReturnListenerToButton(view.getAccountButton(), new AccountOverviewPage());
         //addReturnListenerToButton(view.getTasksButton(), new testpage2("VB"));
         //addReturnListenerToButton(view.getGoalsButton(), new testpage3("VB"));
         //addReturnListenerToButton(view.getManualButton(), new testpage4("VB"));
     }
 
+    /**
+     * Adds an ActionListener to a button.
+     * @param button The button to which the ActionListener will be added.
+     * @param newPage The new page to be displayed when the button is clicked.
+     */
     private void addReturnListenerToButton(JButton button, JFrame newPage) {
         newPage.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         button.addActionListener(new ActionListener() {
