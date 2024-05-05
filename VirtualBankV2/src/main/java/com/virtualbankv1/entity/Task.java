047ee71efce326d84293c2af300a4edfadef1b2e
fsyrachel <fengsiyu0207@gmail.com>
2024-05-05 10:47:20 +0800

    Modify Task: generate the two constructors

--- VirtualBankV2/src/main/java/com/virtualbankv1/entity/Task.java
+++ VirtualBankV2/src/main/java/com/virtualbankv1/entity/Task.java
@@ -4,22 +4,48 @@ package com.virtualbankv1.entity;
 public class Task {
     private String taskID;
     private String description;
+    private String title;
+    private String start;
+    private String end;
+
     private double reward;
     private boolean isCompleted;
+    public static int totalcounter;
     private int counter;
 
-    public Task(String taskID, String description, double reward, boolean isCompleted, int counter) {
-        this.taskID = taskID;
+    public Task(String description, double reward, String title, String start, String end) {
+        this.taskID = String.valueOf(totalcounter);
         this.description = description;
         this.reward = reward;
-        this.isCompleted = isCompleted;
-        this.counter = counter;
+        this.isCompleted = false;
+        this.title = title;
+        this.start = start;
+        this.end = end;
+    }
+
+    public Task(String value, String value1, double v, boolean b, int i, String s, String e, String value2) {
+        this.taskID = value;
+        this.description = value1;
+        this.reward = v;
+        this.isCompleted = b;
+        this.counter = i;
+        this.title = value2;
+        this.start = s;
+        this.end = e;
     }
 
     // 这里可以添加getter和setter方法
     public String getTaskID() {
         return taskID;
     }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title){this.title=title;}
+    public String getStart(){return start;}
+    public void setStart(String start){this.start=start;}
+    public String getEnd(){return end;}
+    public void setEnd(String end){this.end=end;}
 
     public void setTaskID(String taskID) {
         this.taskID = taskID;
