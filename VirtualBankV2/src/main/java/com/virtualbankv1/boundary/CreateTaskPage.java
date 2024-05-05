2b6663b18ee8d3b24ecac3f650263803dce0e497
fsyrachel <fengsiyu0207@gmail.com>
2024-05-05 10:46:44 +0800

    Modify CreateTaskPage: add set/get method, add reward textfield, generate the controller in constructor

--- VirtualBankV2/src/main/java/com/virtualbankv1/boundary/CreateTaskPage.java
+++ VirtualBankV2/src/main/java/com/virtualbankv1/boundary/CreateTaskPage.java
@@ -1,19 +1,30 @@
 package com.virtualbankv1.boundary;
 import com.toedter.calendar.JDateChooser;//需要加入Jcalendar jar到项目路径
+import com.virtualbankv1.control.CreateTaskController;
+
 import javax.swing.*;
 import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class CreateTaskPage extends JFrame{
+    private JTextArea contentArea;
+    private JTextField title;
+    private JTextField reward;
+    private JDateChooser chooseStart;
+    private JDateChooser chooseEnd;
+    private RoundedButton saveButton;
 
     public CreateTaskPage(){
+
         initializeComponents();
+        CreateTaskController controller = new CreateTaskController(this);
+        controller.initializeController();
     }
 
     private void initializeComponents() {
         // 设置窗口大小
-        setSize(new Dimension(1000, 600));
+        setSize(new Dimension(1200, 900));
         setTitle("Virtual Bank");
         // 设置背景颜色
         Color bg = new Color(199, 220, 247);
@@ -24,7 +35,7 @@ public class CreateTaskPage extends JFrame{
 
         //生成组件
         //按钮
-        RoundedButton saveButton = new RoundedButton("Save");
+        saveButton = new RoundedButton("Save");
         saveButton.setBackground(z1); // 设置按钮背景颜色为紫色
         saveButton.setForeground(Color.WHITE); // 设置文本颜色为白色
         //saveButton.setPreferredSize(new Dimension(5, 5)); // 设置按钮的首选大小
@@ -32,13 +43,13 @@ public class CreateTaskPage extends JFrame{
         //Label
         JLabel head = new JLabel("<html><font size=7 color=" + getColorHex(z1) + ">Create Task</font></html>",JLabel.CENTER);
         JLabel l1 = new JLabel("<html><font size=5 color=" + getColorHex(z1) + ">Title*</font></html>",JLabel.LEFT);
-        JLabel l2 = new JLabel("<html><font size=5 color=" + getColorHex(z1) + ">Date*</font></html>",JLabel.LEFT);
+        JLabel l2 = new JLabel("<html><font size=5 color=" + getColorHex(z1) + ">Reward*(/CNY)</font></html>",JLabel.LEFT);
         JLabel l3 = new JLabel("<html><font size=5 color=" + getColorHex(z1) + ">Start time</font></html>",JLabel.LEFT);
         JLabel l4 = new JLabel("<html><font size=5 color=" + getColorHex(z1) + ">End time</font></html>",JLabel.LEFT);
         JLabel l5 = new JLabel("<html><font size=5 color=" + getColorHex(z1) + ">Content</font></html>",JLabel.LEFT);
         JLabel tip = new JLabel("<html><font size=4 color=" + getColorHex(z1) + ">*Required fields</font></html>",JLabel.LEFT);
         //content文本域
-        JTextArea contentArea = new JTextArea();
+        contentArea = new JTextArea();
         contentArea.setLineWrap(true);
         contentArea.setWrapStyleWord(true); // 确保单词不会被拆分
         JScrollPane scrollPane = new JScrollPane(contentArea);
@@ -48,23 +59,15 @@ public class CreateTaskPage extends JFrame{
         scrollPane.setPreferredSize(new Dimension(250, 150));
 
         //文本框
-        JTextField title = new JTextField();
-        JTextField x = new JTextField();
-        JTextField y = new JTextField();
-        JTextField z = new JTextField();
-        //Time
-
-        //创建日期选择器
-        JDateChooser choosedate = new JDateChooser();
-        choosedate.setDateFormatString("yyyy/MM/dd");
-        //Date date = choosedate.getDate();
-        //String strDate = new SimpleDateFormat("yyyy/MM/dd").format(date);
-        JDateChooser chooseStart = new JDateChooser();
-        chooseStart.setDateFormatString("yyyy-MM-dd HH:mm");
+        title = new JTextField();
+        reward = new JTextField();
+        //time
+        chooseStart = new JDateChooser();
+        chooseStart.setDateFormatString("yyyy-MM-dd");
         //Date start = chooseStart.getDate();
         //String strDate = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date);
-        JDateChooser chooseEnd = new JDateChooser();
-        chooseEnd.setDateFormatString("yyyy-MM-dd HH:mm");
+        chooseEnd = new JDateChooser();
+        chooseEnd.setDateFormatString("yyyy-MM-dd");
         //Date end = chooseEnd.getDate();
         //String strDate = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(date);
 
@@ -99,7 +102,7 @@ public class CreateTaskPage extends JFrame{
         C1.add(l1);
         C1.add(title);
         C1.add(l2);
-        C1.add(choosedate);
+        C1.add(reward);
         C1.add(l3);
         C1.add(chooseStart);
         C1.add(l4);
@@ -128,10 +131,40 @@ public class CreateTaskPage extends JFrame{
         setLocationRelativeTo(null); // 居中显示
         //setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
+
+
     }
 
     // 辅助方法：将Color对象转换为HTML颜色字符串
     private String getColorHex(Color color) {
         return String.format("#%02x%02x%02x", color.getRed(), color.getGreen(), color.getBlue());
+    }
+
+    public JTextArea getContentArea() {
+        return contentArea;
+    }
+
+    public JTextField getTitles() {
+        return title;
+    }
+
+    public JTextField getReward() {
+        return reward;
+    }
+
+    public String getStart() {
+        Date s = chooseStart.getDate();
+        String start = new SimpleDateFormat("yyyy-MM-dd").format(s);
+        return start;
+    }
+
+    public String getEnd() {
+        Date e = chooseEnd.getDate();
+        String end = new SimpleDateFormat("yyyy-MM-dd").format(e);
+        return end;
+    }
+
+    public RoundedButton getSaveButton() {
+        return saveButton;
     }
 }
