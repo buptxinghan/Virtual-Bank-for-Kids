9c55841255f6a43c253b924e49d1a0bc745f7f90
fsyrachel <fengsiyu0207@gmail.com>
2024-05-09 20:58:27 +0800

    使得关闭创建页面的窗口也能返回上一页

--- VirtualBankV2/src/main/java/com/virtualbankv2/control/CreateTaskController.java
+++ VirtualBankV2/src/main/java/com/virtualbankv2/control/CreateTaskController.java
@@ -10,6 +10,8 @@ import com.virtualbankv2.entity.Task;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import static com.virtualbankv2.control.VirtualBankApplication.currentUser;
 import static com.virtualbankv2.entity.Task.totalcounter;
@@ -55,7 +57,19 @@ public class CreateTaskController {
 
     public void initializeController() {
         addReturnListenerToButton(view.getSaveButton(), overviewUI);
+        view.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        view.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                returnToTaskOverview();
+            }
+        });
 
+    }
+
+    private void returnToTaskOverview() {
+        view.dispose();
+        overviewUI.setVisible(true);
     }
 
     private void addReturnListenerToButton(RoundedButton button, JFrame newPage) {
